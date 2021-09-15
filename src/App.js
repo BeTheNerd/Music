356450bd64973caf520b4656a1dd420fce54f103
Kelsey Nicholes <bethenerd.dev@gmail.com>
2021-09-15 09:22:08 -0600

    musiclist

--- src/App.js
+++ src/App.js
@@ -1,64 +1,31 @@
-import { Component } from 'react';
-import Song from './Song';
+import styled from "styled-components";
 import Songs from "./Songs";
-import SongForm from "./SongForm";
-import './App.css';
 
-class App extends Component {
-  state = {
-    songs: [
-      { id: 1, artist: "Marina", song: "Oh no!"},
-      { id: 1, artist: "Avril Lavigne", song: "Complicated"},
-      { id: 1, artist: "Fall Out boy", song: "Irresistible"},
-    ],
-  };
-  async componentDidCatch() {
-    console.log("mounted");
-  }
-};
-// add update
-componentDidUpdate(); {
-  console.log("update");
-}
-
-addSong = (song) => {
-  console.log("song");
-  this.setState({
-    songs:[song, ...this.state.songs],
-  });
-};
-
-updateSong = (song) => {
-  let updateSongs = this.state.songs.map((s) =>
-    s.id === song.id ? song : s
-  );
-
-  this.setState({
-    songs: updateSongs,
-  });
-};
-
-deleteSongs = (id) => {
-  let filteredSongs = this.state.songs.filter((s) => s.id !== id);
-  this.setState({
-    songs: filteredSongs,
-  });
-};
-
-render() {
+function App() {
   return (
-    
-        <div>
-        <Song/> 
-        addSong={this.addSong},
-        updateSong={this.updateSong},
-        deleteSong={this.deleteSong},
-        songs={this.state.songs}
-        </div>
-  );
-}
+  <div>
+    <Background>
+    <HeaderText> 
+    <h2>Song!</h2>
+  <Songs/>
+  </HeaderText>
+  </Background>
+  </div>
+
+  )
 };
 
+const HeaderText = styled.h2`
+  color: white !important;
+`;
+const Background = styled.div`
+background: rgb(2,0,36);
+background: linear-gradient(90deg, 
+rgba(2,0,36,1) 0%, 
+rgba(78,9,121,1) 32%, 
+rgba(0,212,255,1) 100%);
+min-height: 100vh;
+`
 
 
 export default App;
--- src/Songs.js
+++ src/Songs.js
@@ -1,20 +1,44 @@
-import React from "react";
-import Song from "./Song";
+import { useState } from "react";
+import { Card, Image, Icon } from "semantic-ui-react";
 
-const Songs = ({ songs, deleteSong, updateSong }) => (
+const Songs = () => {
+  const [songs, setSong] = useState([
 
-<div>
- 
-<>Artist</>
-<>Song</>
+    {id:1, artist: "fall out boy", song: "Irrisitable", link: "https://www.cuindependent.com/new/wp-content/uploads/2018/02/1600px-Fall_out_Boy_Monumentour.jpg"},
+    {id:2, artist: "britney", song: "Lucky", link: "https://www.rollingstone.com/wp-content/uploads/2011/03/britneyspears.jpg?resize=1800,1200&w=1800"},
+    {id:3, artist: "marina", song: "venus fly trap", link: "https://assets.vogue.com/photos/5891ac9afb0604bf1f5c4b32/master/pass/holding-marina-and-the-diamonds.jpg"},
+    {id:4, artist: "kanye", song: "Power", link: "https://media.gq.com/photos/5ad93798ceb93861adb912d8/16:9/w_2560%2Cc_limit/kanye-west-0814-GQ-FEKW01.01.jpg"}
 
-</div>
+  ])
 
-  {songs.map((song) => (
-    <Song
-      updateSong={updateSong}
-      deleteSong={deleteSong}
-      {...song}
-      />
-  ))}
-);
+  const renderSongs = () => {
+    return songs.map((s) => (
+       <div>
+         <Card style={{margin: "20px"}}>
+    <Image src={s.link} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{s.artist}</Card.Header> 
+      <Card.Header>{s.song}</Card.Header> 
+      {/* <p>{s.song}</p>  */}
+    </Card.Content>
+    <Card.Content extra>
+      
+    </Card.Content>
+  </Card>
+        
+        </div>
+      )
+    )
+  }
+
+  return(
+    <div>
+      
+      <h2>List of Songs:</h2>
+      {renderSongs()}
+    </div>
+  );
+};
+
+
+export default Songs
